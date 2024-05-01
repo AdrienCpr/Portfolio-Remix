d0878dcd28ea06fbc9afed5ecbd7033694d019fb
AdrienCpr <adriencompare@gmail.com>
2024-05-02 00:26:34 +0200

    feat: deploy

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,8 +5,10 @@ export const meta: MetaFunction = () => [{ title: "Portfolio - Adrien Compare" }
 export default function Index() {
 
   return (
-    <>
-      <h1>Salut</h1>
-    </>
+    <main className="relative min-h-screen sm:flex sm:items-center sm:justify-center z-10">
+      <div className="relative flex min-h-full justify-center gap-6">
+          <h3>Salut</h3>
+      </div>
+    </main>
   )
 }
